8247999bebf77abd300d32d56695a8c5dae253bd
elsstrange <ellenabstrange@gmail.com>
2020-10-09 12:47:04 +0100

    Refactor to use Bookmark.create in test setup

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,25 +1,12 @@
 feature 'see a list of bookmarks' do
-  let(:expected_bookmark_name1) { 'google' }
-  let(:expected_bookmark_url1) { 'google.co.uk' }
-  let(:expected_bookmark_url2) { 'github.com' }
-  let(:expected_bookmark_name2) { 'github' }
-  #let!(:expected_bookmark_url2) { Bookmark.create(name: 'ed', url: 'YAY') }
+  let(:expected_bookmark_name1) { 'Google' }
+  let(:expected_bookmark_url1) { 'http://www.google.co.uk' }
+  let(:expected_bookmark_url2) { 'Github' }
+  let(:expected_bookmark_name2) { 'http://www.github.com' }
+  let!(:expected_bookmark1) { Bookmark.create(expected_bookmark_name1, expected_bookmark_url1) }
+  let!(:expected_bookmark2) { Bookmark.create(expected_bookmark_name2, expected_bookmark_url2) }
 
   scenario 'visit list' do
-    begin
-      con = PG.connect :dbname => ENV['bookmark_challenge_db'], :user => 'student'
-      list = con.exec("
-        INSERT INTO bookmarks(name,url)
-        VALUES
-          ('#{expected_bookmark_name1}','#{expected_bookmark_url1}'),
-          ('#{expected_bookmark_name2}','#{expected_bookmark_url2}')
-      ")
-    rescue PG::Error => e
-      puts e.message 
-    ensure
-      con.close if con
-    end
-
     visit '/bookmarks'
     expect(page).to have_content expected_bookmark_name1
     expect(page).to have_content expected_bookmark_name2
